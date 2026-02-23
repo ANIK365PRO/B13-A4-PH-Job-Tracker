0d020d4b5c446fe60b99b37d3d43618d4079be47
Anik Mohanta <mohonto05@gmail.com>
2026-02-23 23:29:08 +0600

    added toggleStyle onclick function for corrent coller in button

--- scripts/counter.js
+++ scripts/counter.js
@@ -2,7 +2,7 @@ const interviewList = [];
 const rejectedList = [];
 
 
-//get id to data for call function getID() 
+//get id to data count for call function getID() 
 const totalCount =getId('total_count');
 const interviewCount = getId('interview_count');
 const rejectedCount = getId('rejected_count');
@@ -10,6 +10,10 @@ const rejectedCount = getId('rejected_count');
 // console.log(totalCount.innerText)
 // console.log(totalCount, interviewCount, rejectedCount)
 
+//get id to data filter for call function getID() 
+ const allFilterBtn = getId('all_Filter_Btn');
+ const interviewFilterBtn = getId('interview_Filter_Btn')
+ const rejectedFilterBtn = getId('rejected_Filter_Btn')
 
 // all job container 
 const jobContainer = getId('job_card_container')
@@ -24,3 +28,21 @@ function calculation(){
 }
 
 calculation()
+
+// toggle onclick for filter btn
+function toggleStyle(id){
+    //add common class
+    allFilterBtn.classList.add('bg-base-100', 'text-black');
+    interviewFilterBtn.classList.add('bg-base-100', 'text-black');
+    rejectedFilterBtn.classList.add('bg-base-100', 'text-black');
+
+    // remove default class 
+    allFilterBtn.classList.remove('bg-black', 'text-amber-50');
+    interviewFilterBtn.classList.remove('bg-black', 'text-amber-50');
+    rejectedFilterBtn.classList.remove('bg-black', 'text-amber-50');
+
+    // add current id click class
+    getId(id).classList.remove('bg-base-100', 'text-black')
+    getId(id).classList.add('bg-black', 'text-amber-50')
+
+}
